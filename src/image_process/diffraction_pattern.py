233d82d3edcbc1206b699059428f9a582669c13f
Diego Nogueira <diegofmnogueira@gmail.com>
2024-10-31 12:09:59 -0300

    (fit): implement property mask to eDiffractionPatern

--- src/image_process/diffraction_pattern.py
+++ src/image_process/diffraction_pattern.py
@@ -2,6 +2,15 @@ import numpy as np
 from typing import Tuple
 
 class eDiffractionPattern:
-    def __init__(self, data: np.ndarray, center: Tuple[int,int]):
+    def __init__(self, data: np.ndarray, center: Tuple[int,int], mask: np.ndarray):
         self.data = data
         self.center = tuple(tuple(map(int, center)))
+        self._mask = mask
+    
+    @property
+    def mask(self):
+        return self._mask
+    
+    @mask.setter
+    def mask(self, mask_data):
+        self._mask = mask_data
