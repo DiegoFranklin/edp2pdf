b63d76928eb32f7c88fbd67554cb473233176552
Diego Nogueira <diegofmnogueira@gmail.com>
2024-11-05 14:54:58 -0300

    WIP

--- src/image_process/utils.py
+++ src/image_process/utils.py
@@ -13,17 +13,85 @@ def get_centered_crop_img(image, center):
 
     return image[c_x-radius:c_x+radius, c_y-radius:c_y+radius]
 
-def distance(a, b):
-    """
-    Calculates the Euclidean distance between two points.
-
-    Args:
-        a: A numpy array or tuple representing the first point.
-        b: A numpy array or tuple representing the second point.
-
-    Returns:
-        The Euclidean distance between point a and point b.
-    """
+def euclidean_distance(a, b):
     c = np.subtract(a, b)
 
     return np.linalg.norm(c)
+
+
+def max_radius(data_shape: Tuple[int, int], center: Tuple[int, int]) -> int:
+    """
+    Calculates the maximum radius of a circle centered at the given point, that
+    does not exceed the boundaries of the given data shape.
+
+    Parameters
+    ----------
+    data_shape : Tuple[int, int]
+        Shape of the data.
+    center : Tuple[int, int]
+        Center of the circle.
+
+    Returns
+    -------
+    int
+        The maximum radius of the circle.
+    """
+    corners = [
+        (0, 0), 
+        (0, data_shape[1] - 1),
+        (data_shape[0] - 1, 0),
+        (data_shape[0] - 1, data_shape[1] - 1) 
+    ]
+    # Calculate the maximum distance from the center to the corners
+    max_radius = int(np.ceil(np.max([euclidean_distance(center, corner) for corner in corners])))
+
+    return max_radius
+
+
+class ImagePadder:
+    def __init__(self, data, center, max_radius=None, mode="linear_ramp"):
+        self._data = data
+        self._center = center
+        self._mode = mode
+        self._max_radius = max_radius
+        self._up = None
+        self._down = None
+        self._right = None
+        self._left = None
+        self._square_data = None
+
+    def _compute_max_radius(self):
+        if self._max_radius is None:
+            self._max_radius = max_radius(self._data.shape, self._center)
+
+    def _compute_pad_widths(self):
+        self._up = max(0, self._max_radius - self._center[0])
+        self._down = max(0, self._max_radius - (self._data.shape[0] - self._center[0] - 1))
+        self._left = max(0, self._max_radius - self._center[1])
+        self._right = max(0, self._max_radius - (self._data.shape[1] - self._center[1] - 1))
+
+    def _compute_square_data(self):
+        self._compute_max_radius()
+        self._compute_pad_widths()
+
+        self._square_data = np.pad(
+            self._data,
+            pad_width=(
+                (round(self._up), round(self._down)),
+                (round(self._left), round(self._right))
+            ),
+            mode=self._mode
+        )
+
+    @property
+    def square_data(self):
+        if self._square_data is None:
+            self._compute_square_data()
+        return self._square_data
+
+    def recover_original_shape(self, data):
+        original_data = data[
+            round(self._up):round(data.shape[0] - self._down), 
+            round(self._left):round(data.shape[1] - self._right)
+        ]
+        return original_data
--- src/signal_process/utils.py
+++ src/signal_process/utils.py
@@ -2,10 +2,43 @@ import numpy as np
 from scipy import signal
 
 def cosine_distance(pk, qk):
+    """
+    Compute the cosine distance between two vectors.
+
+    The cosine distance is defined as 1 - cos(angle) where angle is the angle between
+    the two vectors. The cosine of the angle is computed as the dot product of the two
+    vectors divided by their magnitudes.
+
+    Parameters
+    ----------
+    pk, qk : array_like
+        Input vectors.
+
+    Returns
+    -------
+    float
+        The cosine distance between the two vectors.
+    """
     return 1 - np.dot(pk, qk) / (np.linalg.norm(pk) * np.linalg.norm(qk))
 
 def shrink_signal(sig, alpha):
     
+    """
+    Compresses a signal by shrinking it by a factor of alpha while
+    maintaining the same length.
+
+    Parameters
+    ----------
+    sig : array_like
+        Input signal to be shrunk.
+    alpha : float
+        Compression factor.
+
+    Returns
+    -------
+    array_like
+        The compressed signal.
+    """
     original_indices = np.arange(len(sig))
     
     new_indices = np.linspace(0, (len(sig) - 1) * alpha, len(sig))
@@ -15,6 +48,20 @@ def shrink_signal(sig, alpha):
     return compressed_signal
 
 def taper_and_filter(sig):
+    """
+    Apply a Tukey window and a Savitzky-Golay filter to a signal.
+
+    Parameters
+    ----------
+    sig : array_like
+        Input signal.
+
+    Returns
+    -------
+    array_like
+        The filtered signal.
+    """
+
     sig = np.asarray(sig)
     tukey_window = signal.windows.tukey(sig.shape[0], alpha=1)
 
